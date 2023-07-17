20bb551c81fa4356a02a3618fc13123c7fd97611
Nick Ruloff <98500424+nruloff@users.noreply.github.com>
2023-07-17 19:55:53 -0400

    Create create_text_ds.py

---
+++ Functions/create_text_ds.py
@@ -0,0 +1,14 @@
+def create_text_ds(data=train_df):
+  '''
+  INPUT
+  data: pd.Series object;
+
+  OUTPUT
+  text_ds: tf.data.Dataset object;
+
+  '''
+  vectorizer, _ = create_text_vectorizer()
+  ex_phrases = [preprocess_phrase(ind_phrase) for ind_phrase in data['phrase']]
+  text_ds = [vectorizer(ex_phrase) for ex_phrase in ex_phrases]
+  text_ds = tf.data.Dataset.from_tensor_slices(text_ds)
+  return text_ds
